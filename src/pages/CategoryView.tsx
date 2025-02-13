a2495512bef019610a511ef759144974cdf5125c
Joshua Magnase <68670239+joshua0006@users.noreply.github.com>
2025-02-14 10:27:18 +1100

    fix breadcrumb position

--- src/pages/CategoryView.tsx
+++ src/pages/CategoryView.tsx
@@ -42,7 +42,7 @@ export function CategoryView() {
   };
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  ">
+    <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8  ">
       <Breadcrumbs items={breadcrumbItems} />
 
       <h1 className="text-3xl font-bold text-gray-900 mb-8">Tutorials in Category: {categoryName}</h1>
